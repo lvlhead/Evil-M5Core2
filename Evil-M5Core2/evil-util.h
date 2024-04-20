cd83fef9b477013cd9b1b129a24183577e423e73
lvlhead <lvlhe@d>
2024-04-19 22:05:26 -0700

    Global variable header

---
+++ Evil-M5Core2/evil-util.h
@@ -0,0 +1,48 @@
+/*
+    Evil-M5Core2 - WiFi Network Testing and Exploration Tool
+
+    Copyright (c) 2024 7h30th3r0n3
+    Copyright (c) 2024 lvlhead
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+    Disclaimer:
+    This tool, Evil-M5Core2, is developed for educational and ethical testing purposes only.
+    Any misuse or illegal use of this tool is strictly prohibited. The creator of Evil-M5Core2
+    assumes no liability and is not responsible for any misuse or damage caused by this tool.
+    Users are required to comply with all applicable laws and regulations in their jurisdiction
+    regarding network testing and ethical hacking.
+*/
+
+#ifndef EVILUTIL_H
+#define EVILUTIL_H
+
+String ssidList[100];
+int numSsid = 0;
+bool isOperationInProgress = false;
+int currentListIndex = 0;
+String clonedSSID = "Evil-M5Core2";
+int topVisibleIndex = 0;
+
+const byte DNS_PORT = 53;
+unsigned long previousMillis = 0;
+const long interval = 1000;
+static constexpr const gpio_num_t SDCARD_CSPIN = GPIO_NUM_4;
+
+#endif
